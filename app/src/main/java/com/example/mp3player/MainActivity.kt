a42063647b1e384636010d3b5c210f643001cdaf
ismoil <ismoiljoraxonov1@gmail.com>
2025-11-01 23:33:44 +0900

    feat: add media player control buttons and function stubs

--- app/src/main/java/com/example/mp3player/MainActivity.kt
+++ app/src/main/java/com/example/mp3player/MainActivity.kt
@@ -1,15 +1,44 @@
 package com.example.mp3player
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.mp3player.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        binding.btnPlay.setOnClickListener {
+            mediaPlayerPlay()
+        }
+        binding.btnPause.setOnClickListener {
+            mediaPlayerPause()
+        }
+
+        binding.btnStop.setOnClickListener {
+            mediaPlayerStop()
+        }
 
     }
+
+
+    private fun mediaPlayerStop() {
+        TODO("Not yet implemented")
+    }
+
+    private fun mediaPlayerPause() {
+        TODO("Not yet implemented")
+    }
+
+    private fun mediaPlayerPlay() {
+        TODO("Not yet implemented")
+    }
+
 }
